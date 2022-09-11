66adc9b4356dd3f807000af534fbb131fe91bf2b
cbozay <cbozay90@hotmail.com>
2022-09-11 14:28:48 +0300

    adding book process

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,15 +1,64 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const AddBookForm = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState(null);
+  const [bookName, setBookName] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [category, setCategory] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3004/categories")
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (bookName === "" || authorName === "" || category === "") {
+      alert("It can not be empty!");
+      return;
+    }
+
+    const newBook = {
+      id: new Date().getTime(),
+      name: bookName,
+      author: authorName,
+      isbn: isbn,
+      categoryId: category,
+    };
+    axios
+      .post("http://localhost:3004/books", newBook)
+      .then(
+        (res) => setBookName(""),
+        setAuthorName(""),
+        setIsbn(""),
+        setCategory(""),
+        navigate("/")
+      )
+      .catch((err) => console.log(err));
+  };
+  if (categories === null) {
+    return <Loading />;
+  }
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row g-3 my-5">
           <div className="col ">
             <input
               type="text"
               className="form-control"
               placeholder="Book Name"
+              value={bookName}
+              onChange={(event) => setBookName(event.target.value)}
             />
           </div>
           <div className="col">
@@ -17,21 +66,38 @@ const AddBookForm = () => {
               type="text"
               className="form-control"
               placeholder="Author Name"
+              value={authorName}
+              onChange={(event) => setAuthorName(event.target.value)}
             />
           </div>
         </div>
         <div className="row g-3">
           <div className="col">
-            <input type="text" className="form-control" placeholder="ISBN" />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ISBN"
+              value={isbn}
+              onChange={(event) => setIsbn(event.target.value)}
+            />
           </div>
           <div className="col">
-            <select className="form-select">
-              <option selected>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+            <select
+              className="form-select"
+              value={category}
+              onChange={(event) => setCategory(event.target.value)}
+            >
+              <option value={""}>Select a Category</option>
+              {categories.map((category) => {
+                return <option value={category.id}>{category.name}</option>;
+              })}
             </select>
           </div>
+        </div>
+        <div className="d-flex justify-content-center my-5">
+          <button className="btn btn-outline-primary w-25" type="submit">
+            Save
+          </button>
         </div>
       </form>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             Navbar
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,14 +23,9 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link " href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+                <Link className="nav-link " to="/">
+                  Registered Books
+                </Link>
               </li>
             </ul>
           </div>
